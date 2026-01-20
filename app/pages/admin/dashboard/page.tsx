b74f14fa6db664ba16844b9bb15ed096a19a7af6
hafidhayatullah <ahmad.hafidhayatullah@gmail.com>
2026-01-20 12:46:43 +0700

    ### Dashboard - Tambah dashboard admin dan komponen terkait (done) - Tambah dashboard mahasiswa dan komponen terkait (done)

--- app/pages/admin/dashboard/page.tsx
+++ app/pages/admin/dashboard/page.tsx
@@ -1,78 +1,194 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import api from "@/app/lib/axiosInstance";
+import AdminStatCards from "@/app/components/card/AdminStatCard";
+import { BarLoader } from "react-spinners";
+import KrsKhsStatusCard from "@/app/components/card/StatusKrsKhsCard";
+import StudentPerFacultyChart from "@/app/components/card/StudentPerFacultyChart";
+import PaymentCard from "@/app/components/card/PaymentCard";
+import TimelineAkademik from "@/app/components/card/TimelineAkademik";
+import AktivitasKRS from "@/app/components/card/AktivitasKRS";
 
-import React from 'react';
+interface Dashboard {
+  totalStudents: number;
+  totalFaculties: number;
+  totalMajors: number;
+  totalClasses: number;
+  studyPlan: {
+    approved: number;
+    rejected: number;
+    onprocess: number;
+  };
+  studentPerFaculty: [
+    {
+      facultyId: string;
+      facultyName: string;
+      totalStudents: number;
+    }
+  ];
+  payment: {
+    paidCount: number;
+    paidSum: number;
+    unpaidCount: number;
+    unpaidSum: number;
+  };
+  studyPlans: [
+    {
+      courseName: string;
+      studentName: string;
+      lectureName: string;
+      status: string;
+    }
+  ];
+  upcomingTimeline: [
+    {
+      id: string;
+      name: string;
+      date: string;
+      createdAt: string;
+      updatedAt: string;
+    }
+  ];
+}
+
+const getDashboard = async () => {
+  const res = await api.get("/stats/admin");
+  return res.data.data;
+};
 
 const DashboardPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [dashboard, setDashboard] = useState<Dashboard | null>(null);
+
+  // ambil data awal
+  useEffect(() => {
+    const fetchAll = async () => {
+      try {
+        await fetchDashboard();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchAll();
+  }, []);
+
+  const fetchDashboard = async () => {
+    try {
+      const data = await getDashboard();
+      setDashboard(data);
+    } catch (err) {
+      console.error("Gagal fetch dashboard:", err);
+    }
+  };
+
   return (
     <section className="section">
       <div className="section-header">
         <h1>Dashboard</h1>
       </div>
-      {/* <div className="row">
-        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div className="card card-statistic-1">
-            <div className="card-icon bg-primary">
-              <i className="far fa-building"></i>
-            </div>
-            <div className="card-wrap">
-              <div className="card-header">
-                <h4>Total Fakultas</h4>
+      {loading ? (
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{ minHeight: "500px" }}
+        >
+          <BarLoader color="#6777ef" />
+        </div>
+      ) : (
+        <>
+          <div className="row">
+            {/* Bagian kiri */}
+            <div className="col-md-8">
+              <AdminStatCards
+                totalStudents={dashboard?.totalStudents}
+                totalFaculties={dashboard?.totalFaculties}
+                totalMajors={dashboard?.totalMajors}
+                totalClasses={dashboard?.totalClasses}
+                colWidths={[4, 3, 3, 2]}
+              />
+
+              {/* Baris khusus untuk KrsKhsStatusCard + elemen kanan */}
+              <div className="row mt-4">
+                <div
+                  className="col-12 col-md-4 mb-t"
+                  style={{ paddingRight: "5px" }}
+                >
+                  <KrsKhsStatusCard
+                    data={{
+                      disetujui: dashboard?.studyPlan.approved ?? 0,
+                      diproses: dashboard?.studyPlan.onprocess ?? 0,
+                      ditolak: dashboard?.studyPlan.rejected ?? 0,
+                    }}
+                  />
+                </div>
+
+                <div
+                  className="col-12 col-md-8 mb-4"
+                  style={{ paddingLeft: "10px" }}
+                >
+                  {/* Elemen tambahan di kanan KRS status */}
+                  <div className="card p-3" style={{ borderRadius: "0.9rem" }}>
+                    <StudentPerFacultyChart
+                      data={dashboard?.studentPerFaculty || []}
+                    ></StudentPerFacultyChart>
+                  </div>
+                </div>
               </div>
-              <div className="card-body">10</div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div className="card card-statistic-1">
-            <div className="card-icon bg-danger">
-              <i className="fa fa-graduation-cap" style={{ color: 'white', fontSize: 20 }}></i>
-            </div>
-            <div className="card-wrap">
-              <div className="card-header">
-                <h4>Totak Program Studi</h4>
+
+              {/* Baris khusus Aktivitas Kes */}
+              <div
+                className="row"
+                style={{
+                  marginTop: "-50px",
+                  paddingRight: "15px",
+                  paddingLeft: "15px",
+                }}
+              >
+                <AktivitasKRS
+                  studyPlans={dashboard?.studyPlans}
+                  detailLink="/pages/admin/krs"
+                  showScrollbar={false}
+                  divHeight="500px"
+                ></AktivitasKRS>
               </div>
-              <div className="card-body">9</div>
             </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div className="card card-statistic-1">
-            <div className="card-icon bg-warning">
-              <i className="fa fa-chalkboard-teacher" style={{ color: 'white', fontSize: 20 }}></i>
-            </div>
-            <div className="card-wrap">
-              <div className="card-header">
-                <h4>Total Kelas</h4>
+
+            {/* Bagian kanan utama */}
+            <div className="col-md-4">
+              <PaymentCard
+                payment={
+                  dashboard?.payment ?? {
+                    paidCount: 0,
+                    paidSum: 0,
+                    unpaidCount: 0,
+                    unpaidSum: 0,
+                  }
+                }
+              />
+              <div style={{ height: "575px" }}>
+                <TimelineAkademik
+                  upcomingTimeline={dashboard?.upcomingTimeline}
+                  detailLink="admin/time-line"
+                  maxHeight="550px"
+                  showScrollbar={false}
+                />
               </div>
-              <div className="card-body">18</div>
             </div>
           </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div className="card card-statistic-1">
-            <div className="card-icon bg-success">
-              <i className="fa fa-book-reader" style={{ color: 'white', fontSize: 20 }}></i>
-            </div>
-            <div className="card-wrap">
-              <div className="card-header">
-                <h4>Total Mata Kuliah</h4>
+
+          {/* <div className="row">
+            <div className="col-12">
+              <div className="card">
+                <div className="card-header">
+                  <h4>Bar Chart</h4>
+                </div>
+                <div className="card-body"></div>
               </div>
-              <div className="card-body">7</div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <div className="card">
-            <div className="card-header">
-              <h4>Bar Chart</h4>
-            </div>
-            <div className="card-body">
-              <MyBarChart />
-            </div>
-          </div>
-        </div>
-      </div> */}
+          </div> */}
+        </>
+      )}
     </section>
   );
 };
--- app/pages/mahasiswa/dashboard/page.tsx
+++ app/pages/mahasiswa/dashboard/page.tsx
@@ -1,58 +1,195 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
+import api from "@/app/lib/axiosInstance";
+import Cookies from "js-cookie";
+import AktivitasKRS from "@/app/components/card/AktivitasKRS";
+import KelasMengajar from "@/app/components/card/KelasMengajar";
+import TimelineAkademik from "@/app/components/card/TimelineAkademik";
+import AttendanceCard from "@/app/components/card/AttendanceCard";
+import { BarLoader } from "react-spinners";
+
+interface Dashboard {
+  profile: Profile;
+  schedules: Schedule[];
+  studyPlans: StudyPlan[];
+  absensiStudent: AbsensiStudent;
+  upcomingTimeline: UpcomingTimeline[];
+}
+
+interface Profile {
+  id: string;
+  name: string;
+  emai: string;
+}
+
+interface Course {
+  id: string;
+  name: string;
+  code: string;
+  credits: number;
+}
+
+interface Class {
+  id: string;
+  name: string;
+}
+
+interface Schedule {
+  id: string;
+  day: string;
+  courseName: string;
+  courseCredits: number;
+  class: string;
+  timeStart: string;
+  timeEnd: string;
+}
+
+interface Schedules {
+  id: string;
+  day: string;
+  timeStart: string;
+  timeEnd: string;
+  class: Class;
+  course: Course;
+}
+interface StudyPlans {
+  courseName: string;
+  studentName: string;
+  status: string;
+}
+
+interface StudyPlan {
+  name: string;
+  lecture: string;
+  status: string;
+}
+
+interface AbsensiStudent {
+  jumlahHadir: number;
+  jumlahIzin: number;
+  jumlahSakit: number;
+  jumlahAlfa: number;
+}
+
+interface UpcomingTimeline {
+  id: string;
+  name: string;
+  date: string;
+}
+
+const getDashboard = async () => {
+  const res = await api.get("/manage-students/stats");
+  return res.data.data;
+};
 
 const MahasiswaDashboard = () => {
+  const [loading, setLoading] = useState(true);
+  const [dashboard, setDashboard] = useState<Partial<Dashboard>>({});
+  // ambil data awal
+  useEffect(() => {
+    const fetchAll = async () => {
+      try {
+        await fetchDashboard();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchAll();
+  }, []);
+
+  useEffect(() => {
+    if (dashboard?.profile?.name) {
+      Cookies.set("studentName", dashboard?.profile?.name, { expires: 1 });
+      console.log(loading);
+    }
+  }, [dashboard]);
+
+  const fetchDashboard = async () => {
+    try {
+      const data = await getDashboard();
+      setDashboard(data);
+    } catch (err) {
+      console.error("Gagal fetch dashboard:", err);
+    }
+  };
+
+  const mapToStudyPlans = (plan: StudyPlan): StudyPlans => ({
+    courseName: plan.name,
+    studentName: plan.lecture,
+    status: plan.status,
+  });
+
+  const mapToStudySchedules = (plan: Schedule): Schedules => ({
+    id: plan.id,
+    day: plan.day,
+    timeStart: plan.timeStart,
+    timeEnd: plan.timeEnd,
+    class: { id: "", name: plan.class },
+    course: {
+      id: "",
+      name: plan.courseName,
+      credits: plan.courseCredits,
+      code: "",
+    },
+  });
+
+  const mappedPlans = dashboard.studyPlans?.map(mapToStudyPlans);
+  const mappedSchedules = dashboard.schedules?.map(mapToStudySchedules);
   return (
     <section className="section">
       <div className="section-header">
         <h1>Dashboard</h1>
       </div>
-      {/* <div className="row">
-        <div className="col-4">
-          <div className="card card-statistic-1">
-            <div className="card-icon bg-primary">
-              <i className="far fa-user"></i>
+      {loading ? (
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{ minHeight: "500px" }}
+        >
+          <BarLoader color="#6777ef" />
+        </div>
+      ) : (
+        <>
+          <div className="row">
+            {/* KIRI */}
+            <div className="col-md-8">
+              <KelasMengajar
+                schedules={mappedSchedules}
+                detailLink="/pages/dosen/jadwal"
+                showAllData={true}
+                showScrollbar={false}
+                title="Kelas yang diambil"
+              />
+              
+              <AktivitasKRS
+                studyPlans={mappedPlans}
+                detailLink="/pages/mahasiswa/krs"
+                showScrollbar={false}
+                headerMid="Dosen Pengampu"
+                divHeight="655px"
+              />
             </div>
-            <div className="card-wrap">
-              <div className="card-header">
-                <h4>Kartu Rencana Studi Diterima</h4>
-              </div>
-              <div className="card-body">
-                1
-              </div>
+
+            {/* KANAN */}
+            <div className="col-md-4 d-flex flex-column gap-3 mt-3">
+              <AttendanceCard
+                data={
+                  Array.isArray(dashboard.absensiStudent)
+                    ? dashboard.absensiStudent[0]
+                    : dashboard.absensiStudent
+                }
+              />
+              <TimelineAkademik
+                upcomingTimeline={dashboard.upcomingTimeline}
+                detailLink="#"
+                maxHeight="325px"
+                showScrollbar={false}
+              />
             </div>
           </div>
-        </div>
-        <div className="col-4">
-          <div className="card card-statistic-1">
-            <div className="card-icon bg-danger">
-              <i className="far fa-newspaper"></i>
-            </div>
-            <div className="card-wrap">
-              <div className="card-header">
-                <h4>Kartu Rencana Studi Ditolak</h4>
-              </div>
-              <div className="card-body">
-                0
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-4">
-          <div className="card card-statistic-1">
-            <div className="card-icon bg-warning">
-              <i className="far fa-file"></i>
-            </div>
-            <div className="card-wrap">
-              <div className="card-header">
-                <h4>Total Pembayaran</h4>
-              </div>
-              <div className="card-body">
-                Rp 2.956.764,-
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
+        </>
+      )}
     </section>
   );
 };
